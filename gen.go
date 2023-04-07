88d059dbe608697b7746e7e8a898bcb2373485ec
runnart <ggasymov@ozon.ru>
2023-04-07 23:39:19 +0400

    (upd): rename package to go_openaiclient

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package go_openaiclient
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --debug.noerr --clean --package openaiclient --target . _oas/schema.yaml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --debug.noerr --clean --package go_openaiclient --target . _oas/schema.yaml
